6f8682bd59a21ea24008ce448d793896b0f833c7
teopaulo <pablo.rosales14@gmail.com>
2022-07-06 11:20:17 -0700

    successfully added the correct characters to a holding array for password criteria

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function generatePassword(){
 
   var password;
-  var lowerCases = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var upperCases = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  var numbers = [0,1,2,3,4,5,6,7,8,9];
-  var specialChars = ["!","@","#","$","%","^","&","*","(",")"];
-  var allowedChars = [];
+  
   var passwordLength = getPwdL();
   getCriteria();
-  
+
   return password;
 }
 
@@ -33,6 +29,11 @@ function getCriteria(){
   var useUpperC = confirm("Do you want uppercase characters in your password?");
   var useNumbers = confirm("Do you want numbers in your password?");
   var useSpecialC = confirm("Do you want special characters in your password?");
+
+  console.log("Lowercases: ", useLowerC);
+  console.log("Uppercases: ", useUpperC);
+  console.log("Numbers: ", useNumbers);
+  console.log("Special characters: ", useSpecialC);
   
   // if they answer everything as false, ask the questions again
   if(useLowerC === false && useUpperC === false && useNumbers === false && useSpecialC === false) {
@@ -41,9 +42,12 @@ function getCriteria(){
 
   // if they answered with one criteria, then we can use it in the generator
   } else {
+
+    var allowedChars = [];
     
     // lowercase activation 
     if(useLowerC){
+      var lowerCases = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
        lowerCases.forEach(element => {
          allowedChars.push(element)
        });
@@ -51,6 +55,7 @@ function getCriteria(){
 
     // uppercase activation 
     if(useUpperC){
+      var upperCases = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
       UpperCases.forEach(element => {
         allowedChars.push(element)
       });
@@ -58,6 +63,7 @@ function getCriteria(){
 
     // numbers activation 
     if(useNumbers){
+      var numbers = [0,1,2,3,4,5,6,7,8,9];
       numbers.forEach(element => {
         allowedChars.push(element)
       });
@@ -65,13 +71,14 @@ function getCriteria(){
 
     // special characters activation 
     if(useSpecialC){
+      var specialChars = ["!","@","#","$","%","^","&","*","(",")"];
       specialChars.forEach(element => {
         allowedChars.push(element)
       });
     }
 
   }
-  
+  console.log(allowedChars);
 }
 
 
